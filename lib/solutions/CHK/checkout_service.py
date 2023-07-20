2a1a8541fd77c8183be162b6dcf52c2ea5d129b1
sbx_user1051 <sbx_user1051@169.254.18.125>
2023-07-20 14:28:08 +0000

    Thu Jul 20 14:28:08 UTC 2023

--- lib/solutions/CHK/checkout_service.py
+++ lib/solutions/CHK/checkout_service.py
@@ -2,6 +2,16 @@
 Service for handling checkouts.
 """
 
+import itertools
+from typing import TypeAlias
+
+
+sku_pricesT: TypeAlias = dict[str, int]
+sku2quantT: TypeAlias = dict[str, int]
+sku_offersT: TypeAlias = dict[int, int]
+offersT: TypeAlias = dict[str, sku_offersT]
+free_itemsT: TypeAlias = dict[str, tuple[int, str]]
+
 
 class CheckoutService:
     """
@@ -10,34 +20,98 @@ class CheckoutService:
 
     # We'll normally want these in a DB or some file.
     # Prices: SKU -> price
-    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
-    # Offers: SKU -> (quantity, price_for_that_quantity)
-    offers = {"A": (3, 130), "B": (2, 45)}
+    prices: sku_pricesT = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+    # Offers: SKU -> {quantity -> price_for_that_quantity}
+    offers: offersT = {"A": {3: 130, 5: 200}, "B": {2: 45}}
+    free_items: free_itemsT = {"E": (2, "B")}
 
     def __init__(self) -> None:
         pass
+
+    def _apply_free(self, sku2quant: sku2quantT) -> None:
+        """
+        Return `sku2quant` with free items' quantities reduced appropriately.
+
+        Strictly speaking, overlapping offers could cause conflicts. For
+        example, 2E could give a free C (value 20), while 3E could give a free
+        B (value 30). So, what would 6E give?
+
+        Further, these are items, and a customer would want to pick free items.
+        This means that even if the prices were C=19, B=30, i.e., 2B being
+        worth more than 3C, we shouldn't make this pick automatically.
+
+        Therefore, we assume that such conflicts do not exist, as "offers are
+        well balanced so that they can be safely combined".
+        """
+        for sku, quantity in sku2quant.items():
+            try:
+                free_quantity, free_sku = self.free_items[sku]
+            except KeyError:
+                pass
+            else:
+                free_item_quant = sku2quant.get(free_sku, 0)
+                free_item_quant -= quantity // free_quantity
+                if free_item_quant > 0:
+                    sku2quant[free_sku] = free_item_quant
+                else:
+                    # TODO: Add extra free items to the basket?
+                    sku2quant.pop(free_sku, None)
+
+    def get_best_price(
+        self, price: int, quantity: int, sku_offers: sku_offersT,
+    ) -> int:
+        """
+        Return the optimal price for the given quantity of items.
+
+        Note that it is not always directly clear which discounts to apply. For
+        example,
+
+        * 6A can be 2 * 3A = 260 or 1 * 5A + A = 250, implying that we might
+          prefer larger offers;
+
+        * 9A can be 3 * 3A = 390 or 1 * 5A + 4 * A = 400, implying greedy
+          approach.
+
+        Since baskets are usually small and offers rarely very plentiful, we'll
+        just try all possibilities and optimise later on if needed.
+
+        :param price: Individual item's price (without any discounts).
+        :param quantity: The quantity of the given item in the basket.
+        :param sku_offers: A dictionary with all offers for that item.
+        :return: The best price for the items.
+        """
+        result = quantity * price
+        if not sku_offers:
+            return result
+
+        # Apply max. of each discount, in all possible orderings.
+        for discount_counts in itertools.permutations(sku_offers):
+            total_price = 0
+            remaining_quantity = quantity
+            for discount_cnt in discount_counts:
+                discount_quantity = remaining_quantity // discount_cnt
+                if discount_quantity:
+                    total_price += discount_quantity * sku_offers[discount_cnt]
+                    remaining_quantity -= discount_quantity
+                    if not remaining_quantity:
+                        break
+            total_price += remaining_quantity * price
+            if total_price < result:
+                result = total_price
+
+        return result
 
     def get_item_price(self, sku: str, quantity: int) -> int:
         """
         Return the price for `quantity` number of items defined by `sku`.
         """
-        result = 0
         try:
             price = self.prices[sku]
         except KeyError:
             raise ValueError(f"invalid SKU: {repr(sku)}")
 
-        try:
-            offer = self.offers[sku]
-        except KeyError:
-            pass
-        else:
-            result += (quantity // offer[0]) * offer[1]
-            quantity %= offer[0]
-
-        result += quantity * price
-
-        return result
+        sku_offers = self.offers.get(sku, dict())
+        return self.get_best_price(price, quantity, sku_offers)
 
     def get_basket_price(self, basket: str) -> int:
         """
@@ -47,7 +121,7 @@ class CheckoutService:
         :return: The price of the basked with given SKUs, or -1 if some SKU is
             invalid (i.e., it does not exist in service's `prices` dictionary).
         """
-        sku2quant: dict[str, int] = dict()
+        sku2quant: sku2quantT = dict()
         # Note: wrong specs. It said that SKUs are "individual letters of the
         # alphabet".
         for sku in basket:
@@ -59,3 +133,4 @@ class CheckoutService:
             )
         except ValueError:
             return -1
+
