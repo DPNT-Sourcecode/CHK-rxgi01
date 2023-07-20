edf644adfaa3933e4b168889fa9ceac68f9ff82b
sbx_user1051 <sbx_user1051@169.254.18.125>
2023-07-20 12:07:15 +0000

    Thu Jul 20 12:07:15 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,13 @@
+"""
+Solution for `SUM` challenge.
+"""
+
+
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
     """
     Return the sum of `x` and `y`.
     """
     return x + y
+
 
