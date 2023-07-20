82fbb0a13d752b6fb8f39b2f904eb80b7ba772e4
sbx_user1051 <sbx_user1051@169.254.18.125>
2023-07-20 12:13:38 +0000

    Thu Jul 20 12:13:38 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,15 @@
+"""
+Solution for the `HLO` challenge.
+"""
+
+from typing import Any
 
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: Any) -> str:
+    """
+    Return a "Hello World" message.
+    """
+    return "Hello, World!"
+
